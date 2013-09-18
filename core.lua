65d511c7ac836140acb3f9b80fd575571ea4e570
Haleth <haleth@telenet.be>
2013-09-18 11:51:42 +0200

    Attempt to fix treasure cache detection.

--- core.lua
+++ core.lua
@@ -1,7 +1,13 @@
 local f = CreateFrame("Frame")
 f:RegisterEvent("VIGNETTE_ADDED")
 f:SetScript("OnEvent", function(self, event, vignetteInstanceID)
-	if vignetteInstanceID == 989921295 then return end -- Kukuru's Treasure Cache
+	if vignetteInstanceID then
+		local _, _, name = C_Vignettes.GetVignetteInfoFromInstanceID(vignetteInstanceID)
+
+		if name and name == "Kukuru's Treasure Cache" then
+			return
+		end
+	end
 
 	PlaySoundFile("Sound\\Interface\\RaidWarning.wav")
 	RaidNotice_AddMessage(RaidWarningFrame, "Rare spotted!", ChatTypeInfo["RAID_WARNING"])
