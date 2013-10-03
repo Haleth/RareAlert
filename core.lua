3c40292fdc516ddcc0768e43f00c32ca01d99271
Haleth <haleth@telenet.be>
2013-10-03 20:06:30 +0200

    Remove Kukuru detection code.
    It's no longer needed.

--- core.lua
+++ core.lua
@@ -1,20 +1,6 @@
-﻿local kukuru = "Kukuru's Treasure Cache"
-
-if GetLocale() == "ruRU" then
-	kukuru = "Клад Кукуру"
-end
-
-local f = CreateFrame("Frame")
+﻿local f = CreateFrame("Frame")
 f:RegisterEvent("VIGNETTE_ADDED")
-f:SetScript("OnEvent", function(self, event, vignetteInstanceID)
-	if vignetteInstanceID then
-		local _, _, name = C_Vignettes.GetVignetteInfoFromInstanceID(vignetteInstanceID)
-
-		if name and name == kukuru then
-			return
-		end
-	end
-
+f:SetScript("OnEvent", function()
 	PlaySoundFile("Sound\\Interface\\RaidWarning.wav")
 	RaidNotice_AddMessage(RaidWarningFrame, "Rare spotted!", ChatTypeInfo["RAID_WARNING"])
 end)
