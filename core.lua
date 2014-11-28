006b4af6793fa42042e3e8d98a16a0ef0bcfa17d
Haleth <haleth@telenet.be>
2014-11-29 00:35:27 +0100

    Stop spam while in garrison.

--- core.lua
+++ core.lua
@@ -1,6 +1,13 @@
+local blacklist = {
+	[971] = true, -- Alliance garrison
+	[976] = true, -- Horde garrison
+}
+
 local f = CreateFrame("Frame")
 f:RegisterEvent("VIGNETTE_ADDED")
 f:SetScript("OnEvent", function()
+	if blacklist[GetCurrentMapAreaID()] then return end
+
 	PlaySoundFile("Sound\\Interface\\RaidWarning.wav")
 	RaidNotice_AddMessage(RaidWarningFrame, "Rare spotted!", ChatTypeInfo["RAID_WARNING"])
 end)
